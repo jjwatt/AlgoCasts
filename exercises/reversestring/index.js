36291537d8dcac53ff458a5685eeb2113b81c790
Jesse J. Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-02-08 15:31:08 -0500

    Add my recursive solition for the first exercise, reversestring.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+    // return str.split('').reduce((reversed, character) =>
+    //     character + reversed
+    //                             , '');
+
+    // let reversed = '';
+    // for (let i of str) {
+    //     reversed = i + reversed;
+    // }
+    // return reversed;
+
+	// let outputStr = '';
+	// for (let i = str.length; i > 0; --i) {
+	// 	outputStr = "".concat(outputStr, str[i-1]);
+	// }
+	  // return String(outputStr);
+
+    // Recursive/Functional
+    return revs_aux(str, [], 0);
+}
+
+// Functional helper
+function revs_aux(strin, list, index) {
+    if (list.length == strin.length) {
+        return list;
+    }
+    else {
+        return revs_aux(strin,
+                        strin[index] + list,
+                        index+1);
+    }
+}
 
 module.exports = reverse;
